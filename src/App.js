50ddbc1c5f1a2064f0c23a74c8646d5eeb63d0c6
Mohammadreza <mohamadrezaahi2003@gmail.com>
2022-12-21 16:01:15 +0330

    App is done v1.1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ const App = () => {
   const [data, setData] = useState(null);
   const [name, setName] = useState("");
   const [error, setError] = useState("");
-
   const changeHandler = (event) => {
     const value = event.target.value;
     setName(value);
@@ -18,43 +17,55 @@ const App = () => {
     }
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=7e4dc5d9621c65a85ad1f36c13575bb9&units=metric`)
       .then((response) => {
-        console.log(1);
-        console.log(response);
         setData(response.data)
-        console.log(2);
       })
       .catch((error) => {
-        console.log(3);
-        console.log(error);
         setError(error.response.data.cod);
-        console.log(4);
       })
   }
 
 
   return (
-    <div>
-      <div className="top-banner">
-        <div className="container">
-          <h1 className="heading">Weather App</h1>
-          <form>
-            <input type="text" placeholder="Search for a city" onChange={changeHandler} />
-            <button onClick={clickHandler}>Search</button>
-            <div className="msg">
-              {
-                error === "404" && <span>{error.response.data.message}</span>
-              }
+    <>
+      {
+        data === null
+          ?
+          <section className="top-banner">
+            <div className="container">
+              <h1 className="heading">Weather App</h1>
+              <form>
+                <input type="text" placeholder="Search for a city" onChange={changeHandler} />
+                <button onClick={clickHandler}>Search</button>
+                <span className="msg">
+                  {
+                    error === "404" && <p>City not found !!!</p>
+                  }
+                </span>
+              </form>
             </div>
-          </form>
-          <br />
-          <div className="data">
-            {
-              data !== null && <h1 className="h1N">Name: {data.name} , Temperature: {data.main.temp}</h1>
-            }
-          </div>
-        </div>
-      </div>
-    </div>
+          </section>
+          :
+
+          <section className="data-section">
+            <div className="container">
+              <ul className="citys">
+                <li className="city">
+                  <h1 className="city-name">{data !== null && <p>{data.name} {data.sys.country}</p>}</h1>
+                  <div className="city-temp">{data !== null && <h1>{Math.floor(data.main.temp)}</h1>}</div>
+                  <figure>
+                    <img className="city-icon" src={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.weather[0]["icon"]}.svg`} alt="city" />
+                    <p>{data.weather[0]["description"]}</p>
+                  </figure>
+                </li>
+              </ul>
+            </div>
+          </section>
+
+
+      }
+
+    </>
+
   );
 };
 
